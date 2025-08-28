3ea841dec9ccb2b1500a9fc4bd2fa689b505a898
Erik Strandler <erik@court22.com>
2025-08-28 22:13:06 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Risk It!",
   description: "A thrilling betting game",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🍪</text></svg>",
+  },
 };
 
 export const viewport: Viewport = {
